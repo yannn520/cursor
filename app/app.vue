3e86c0a95db0e4059e173b76c4ab51c1c75f0756
Oliver Mak <oliver139.working@gmail.com>
2026-08-21 02:19:29 +0800

    Some Progress

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,14 @@
       <NuxtPage />
     </UMain>
 
-    <SiteBackground />
+    <UFooter class="text-primary-700">
+      <span>
+        <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.zh-hant" target="_blank">CC BY-NC-ND 4.0</a>
+        2026–PRESENT © 黫桃小丸子
+      </span>
+    </UFooter>
+
+    <!-- <SiteBackground /> -->
   </UApp>
 </template>
 
--- app/components/SectionBox.vue
+++ app/components/SectionBox.vue
@@ -1,12 +1,11 @@
 <template>
-  <UContainer>
-    <SectionBox>
-      asd
-    </SectionBox>
-  </UContainer>
+  <div class="mt-10 rounded-2xl bg-neutral-50/50 p-6">
+    <slot />
+  </div>
 </template>
 
 <script setup lang="ts">
+
 </script>
 
 <style scoped>
