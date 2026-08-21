5bf997c9df0cefa501ffe85ab9f85748f22054ea
Oliver Mak <oliver.mak@naxos.com>
2026-08-21 14:52:47 +0800

    Adjust size

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <UContainer class="pt-10 pb-4">
     <h1 class="sr-only">黫桃小丸子的小游標</h1>
-    <SectionBox class="flex items-center gap-12">
+    <SectionBox class="flex items-center gap-4 transition-[gap] md:gap-12">
       <h2 class="sr-only">畫師的資料</h2>
-      <div><NuxtImg src="/images/profile.jpg" class="w-40 rounded-full" alt="畫師的頭像" /></div>
+      <div><NuxtImg src="/images/profile.jpg" class="w-32 rounded-full transition-all md:w-40" alt="畫師的頭像" /></div>
       <div>
-        <p class="text-3xl font-medium text-primary-700">黫桃小丸子</p>
+        <p
+          class="
+            text-[1.75rem] font-medium whitespace-nowrap text-primary-700 transition-all
+            md:text-3xl
+          "
+        >
+          黫桃小丸子
+        </p>
         <ul class="mt-2 flex items-center gap-2 text-[2.5rem]">
           <li>
             <a href="https://www.facebook.com/qin.yan.521963" target="_blank" class="flex text-primary-800/60 transition-colors hover:text-primary-800">
@@ -50,8 +57,8 @@
       </NuxtLink>
       <div
         class="
-          grid grid-col-auto-fill-36 items-center justify-items-center gap-x-4
-          gap-y-8
+          grid grid-cols-2 items-center justify-items-center gap-y-8
+          md:grid-cols-4
         "
       >
         <NuxtImg v-for="i in 16" :key="i" :src="`/images/cursors-gif/${i}.gif`" />
